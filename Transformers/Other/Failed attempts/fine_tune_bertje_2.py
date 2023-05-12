d644d1faa7a77ccd4e2e9b13f0197f5cec82bb71
Noah-Manuel Michael <n.michael@student.vu.nl>
2023-05-12 19:17:00 +0200

    Reorganized

--- Transformers/Other/Failed attempts/fine_tune_bertje_2.py
+++ Transformers/Other/Failed attempts/fine_tune_bertje_2.py
@@ -13,7 +13,7 @@ def main():
     tokenizer = BertTokenizer.from_pretrained("GroNLP/bert-base-dutch-cased")
     model = BertforTokenReorganization.from_pretrained("GroNLP/bert-base-dutch-cased")
 
-    dataset = SentenceReorderingDataset('../../Data/Dataset Construction/Data/readability_data_scrambled.tsv', tokenizer)
+    dataset = SentenceReorderingDataset('../../../Data/Dataset Construction/Data/readability_data_scrambled.tsv', tokenizer)
     train_size = int(0.9 * len(dataset))
     val_size = len(dataset) - train_size
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
