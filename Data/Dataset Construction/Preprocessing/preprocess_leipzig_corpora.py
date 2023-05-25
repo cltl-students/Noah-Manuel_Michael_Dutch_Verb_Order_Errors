8b7da599c6ef0a4cca6bef47e0be617537b980a8
Noah-Manuel Michael <n.michael@student.vu.nl>
2023-05-25 13:29:18 +0200

    Split datasets

--- Data/Dataset Construction/Preprocessing/preprocess_leipzig_corpora.py
+++ Data/Dataset Construction/Preprocessing/preprocess_leipzig_corpora.py
@@ -7,32 +7,26 @@ import re
 import pandas as pd
 
 
-df_mixed = pd.read_csv('../Unpermuted Datasets/leipzig_data.tsv',
-                           sep='\t', encoding='utf-8', header=0)
+def preprocess_leipzig_corpora():
+    """
 
-print(df_mixed)
+    :return:
+    """
+    df_mixed = pd.read_csv('../Unpermuted Datasets/Originals/leipzig_corpus/nld_mixed_2012_1M-sentences.txt',
+                           sep='\t', encoding='utf-8', names=['index', 'sent'])
+    df_news = pd.read_csv('../Unpermuted Datasets/Originals/leipzig_corpus/nld_news_2022_1M-sentences.txt',
+                          sep='\t', encoding='utf-8', names=['index', 'sent'])
+    df_wiki = pd.read_csv('../Unpermuted Datasets/Originals/leipzig_corpus/nld_wikipedia_2021_1M-sentences.txt',
+                          sep='\t', encoding='utf-8', names=['index', 'sent'])
 
-#
-# def preprocess_leipzig_corpora():
-#     """
-#
-#     :return:
-#     """
-#     df_mixed = pd.read_csv('../Unpermuted Datasets/Originals/leipzig_corpus/nld_mixed_2012_1M-sentences.txt',
-#                            sep='\t', encoding='utf-8', names=['index', 'sent'])
-#     df_news = pd.read_csv('../Unpermuted Datasets/Originals/leipzig_corpus/nld_news_2022_1M-sentences.txt',
-#                           sep='\t', encoding='utf-8', names=['index', 'sent'])
-#     df_wiki = pd.read_csv('../Unpermuted Datasets/Originals/leipzig_corpus/nld_wikipedia_2021_1M-sentences.txt',
-#                           sep='\t', encoding='utf-8', names=['index', 'sent'])
-#
-#     all_sents = {sent for sent in (df_wiki['sent'].tolist() + df_mixed['sent'].tolist() + df_news['sent'].tolist())
-#                  if len(sent) > 10 and re.match(r'^[A-Z].*[.!?]$', sent)}
-#
-#     with open('../Unpermuted Datasets/leipzig_data.tsv', 'w', encoding='utf-8') as outfile:
-#         outfile.write('index\toriginal\n')
-#         for i, sent in enumerate(all_sents):
-#             outfile.write(f'{i}\t{sent}\n')
-#
-#
-# if __name__ == '__main__':
-#     preprocess_leipzig_corpora()
+    all_sents = {sent for sent in (df_wiki['sent'].tolist() + df_mixed['sent'].tolist() + df_news['sent'].tolist())
+                 if len(sent) > 10 and re.match(r'^[A-Z].*[.!?]$', sent)}
+
+    with open('../Unpermuted Datasets/leipzig_data.tsv', 'w', encoding='utf-8') as outfile:
+        outfile.write('index\toriginal\n')
+        for i, sent in enumerate(all_sents):
+            outfile.write(f'{i}\t{sent}\n')
+
+
+if __name__ == '__main__':
+    preprocess_leipzig_corpora()
